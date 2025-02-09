bfae1b6de9ddb7aa2cd25180d1028d64065eee70
vladcomarlau <vladcom95@gmail.com>
2025-02-10 01:40:31 +0200

    fix webVitals error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const logo = require("./logo.svg") as string;
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +18,7 @@ function App() {
         >
           Comarlau.com
         </a>
+        <a>App 1</a>
       </header>
     </div>
   );
