71e10adc2022ef2406420b79a911390a8f216097
vladcomarlau <vladcom95@gmail.com>
2025-04-13 14:03:02 +0300

    add BrowserRouter to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <BrowserRouter>  
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
